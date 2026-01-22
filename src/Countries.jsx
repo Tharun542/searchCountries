1706261ad39da69bd079498d7e404c02b3a7bda4
tharun <godasutharun143@gmail.com>
2026-01-22 11:27:39 +0530

    error resolved

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,37 +1,63 @@
 import React, { useEffect, useState } from "react";
 import Cart from "./Cart";
-import "./countries.css"
+import "./countries.css";
 
-export default function Countries(){
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+    const [search, setSearch] = useState("");
 
-    const [countryFlag, setCountryFlag] = useState([]);
-    const [search, setSearch] = useState('');
+    useEffect(() => {
+        const fetchCountries = async () => {
+            try {
+                const res = await fetch(
+                    "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+                );
+                const data = await res.json();
+                setCountries(data);
+            } catch (error) {
+                console.error("Error to fetching the countryFlags");
+            }
+        };
+        fetchCountries();
+    }, []);
 
-    
-   useEffect(()=>{
-    const upi = async()=>{
-        try{
-          const url = await fetch("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
-            let urlCountry = await url.json();
-            setCountryFlag(urlCountry);
-        }
-        catch(error){
-            console.error("Error to fetching the countryFlags");
-        } 
-    }
-    upi();
-   },[])
+    const filteredCountries = countries.filter((country) => {
+        const name =
+            country.common ||
+            country.name?.common ||
+            "";
 
+        return name.toLowerCase().includes(search.toLowerCase());
+    });
 
-    return(
+    return (
         <div>
-            <input type="text" value={search} placeholder="search for countries" onChange={(e)=> setSearch(e.target.value)} />
+            <input
+                type="text"
+                placeholder="search for countries"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+
             <div className="flag">
-            {countryFlag.filter((item)=> item.common.toLowerCase().includes(search.toLocaleLowerCase())).map((item)=>(
-                    <Cart key={item.common} image={item.png} name={item.common} />
-            ))}
+                {filteredCountries.map((country) => {
+                    const name =
+                        country.common ||
+                        country.name?.common;
+
+                    const image =
+                        country.png ||
+                        country.flags?.png;
+
+                    return (
+                        <Cart
+                            key={name}
+                            image={image}
+                            name={name}
+                        />
+                    );
+                })}
+            </div>
         </div>
-        </div>
-        
-    )
+    );
 }
