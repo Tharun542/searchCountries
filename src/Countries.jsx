1dccf5bf1b0c2ef5cc2393932933a4ac73236580
tharun <godasutharun143@gmail.com>
2026-01-22 11:17:28 +0530

    updated successfully searchcountries

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -11,7 +11,7 @@ export default function Countries(){
    useEffect(()=>{
     const upi = async()=>{
         try{
-          const url = await fetch(" https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
+          const url = await fetch("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
             let urlCountry = await url.json();
             setCountryFlag(urlCountry);
         }
@@ -28,9 +28,7 @@ export default function Countries(){
             <input type="text" value={search} placeholder="search for countries" onChange={(e)=> setSearch(e.target.value)} />
             <div className="flag">
             {countryFlag.filter((item)=> item.common.toLowerCase().includes(search.toLocaleLowerCase())).map((item)=>(
-                <div key={item.common}>
-                    <Cart image={item.png} name={item.common} />
-                </div>
+                    <Cart key={item.common} image={item.png} name={item.common} />
             ))}
         </div>
         </div>
